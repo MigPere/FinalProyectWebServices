0b18dedee450b730a37e7f3e3e6af70a09b89486
migueperez <yosoygroott56@gmail.com>
2022-03-26 17:47:56 -0600

    agregando carpeta js dentro de la carpeta FinalProjectWebServices

--- index.php
+++ index.php
@@ -1,25 +1,42 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/03a89292db.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/estilos.css">
     <title>Document</title>
 </head>
+
 <body>
     <!--agregando foto de perfil-->
-<section>
-<div class="card">
-    <div class="head">
-        <div class="img">
-            <img src="" alt="">
-    </div>
-    </div>
+    <section class="card">
+        <div class="card__perfil">
+            <div class="card__nombre">
+                <img src="image/alexander.jpg" alt="">
+                <h2>Rajnish Kumar</h2>
+                <p>Diseñador web</p>
+            </div>
+            <hr>
+            <div class="card__descripcion">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita dolores perferendis rem ipsam
+                    veniam reiciendis eveniet optio eum velit, dignissimos asperiores fugit molestias ipsum temporibus
+                    rerum hic iste nulla vitae?</p>
+            </div>
+            <hr>
+            <div class="card__button">
+                <a class="enlace" href="#">Saber más</a>
+            </div>
 </div>
-
-</section>
+    </section>
     
+    
+    <!--redes sociales-->
+
+
 </body>
+
 </html>
